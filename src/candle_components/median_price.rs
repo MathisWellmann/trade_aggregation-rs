a513a13d3f0df0892d84599aa05f91296a59b688
MathisWellmann <wellmannmathis@gmail.com>
2022-09-06 16:42:20 +0200

    added MedianPrice CandleComponent

---
+++ src/candle_components/median_price.rs
@@ -0,0 +1,41 @@
+use crate::CandleComponent;
+
+/// Computes the median price from a sorted list of trade prices
+#[derive(Debug, Default, Clone)]
+pub struct MedianPrice {
+    prices: Vec<f64>,
+}
+
+impl CandleComponent for MedianPrice {
+    #[inline(always)]
+    fn value(&self) -> f64 {
+        let mut prices = self.prices.clone();
+        prices.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        prices[prices.len() / 2]
+    }
+
+    #[inline(always)]
+    fn update(&mut self, trade: &crate::Trade) {
+        self.prices.push(trade.price);
+    }
+
+    #[inline(always)]
+    fn reset(&mut self) {
+        self.prices.clear();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn median_price() {
+        let mut m = MedianPrice::default();
+        for t in &crate::candle_components::tests::TRADES {
+            m.update(t);
+        }
+        assert_eq!(m.value(), 102.0);
+    }
+}
